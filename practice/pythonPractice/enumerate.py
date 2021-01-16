f67d19409ee8be816fd4a48035bf7ac2cd8eff86
Juyong Hong <juyong3525@gmail.com>
2021-01-16 19:38:52 +0900

    Added note and enumerate

---
+++ practice/pythonPractice/enumerate.py
@@ -0,0 +1,9 @@
+data = {
+    'name': 'yong',
+    'fav_food': 'kimchi',
+    'age': '20',
+    'gender': 'unknown'
+}
+
+for index, key in enumerate(data):
+    print(index, key, data[key])
---
+++ practice/pythonPractice/note.py
@@ -0,0 +1,9 @@
+r = 10  # 반지름이 10인 변수 r
+pi = 3.141592   # 원주율(pi)
+
+diameter = 2 * r    # 지름(2 X 반지름)
+circumference = pi * diameter   # 원의 둘레
+area = pi * r ** 2
+
+print(diameter, circumference, area)
+# 20, 62.83184, 314.1592
