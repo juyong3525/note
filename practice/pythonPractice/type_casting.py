6241243fefb7a0a802fa815e8a162fb19ab3ded8
Juyong Hong <juyong3525@gmail.com>
2021-01-17 13:36:32 +0900

    Added type casting

---
+++ practice/pythonPractice/type_casting.py
@@ -0,0 +1,24 @@
+# 1
+# int_num = 10	# 정수형 변수
+# float_num = float(int_num)	# float() 소수로 변환
+# print(float_num)
+
+
+# 2
+# str_num = "10.11"  # 문자형 변수
+# float_num = float(str_num)    # float() 소수로 변환
+# print(float_num)
+# print(type(float_num))
+
+
+# 3
+# float_num = 9.99  # 소수형 변수
+# int_num = int(float_num)    # int() 정수로 변환
+# print(int_num)
+
+
+# 4
+# str_num = "10"  # 문자형 변수
+# int_num = int(str_num)    # int() 정수로 변환
+# print(int_num)		# int_num 출력
+# print(type(int_num))
